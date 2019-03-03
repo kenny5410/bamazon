836f6874bb0707444c3c497ccc9956e15bc309d5
Kenny <nels5410@gmail.com>
2019-03-03 17:17:51 -0600

    Replaced Screenshots

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
+//Node Package Managers used in this application
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table");
 
+//Creation of the mySQL database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -13,19 +15,22 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
+//Connection function which also runs the start function
 connection.connect(function(err) {
     if (err) throw err;
     start();
 });
 
-
+//Start function which displays the table and starts the inquirer questions
 function start() {
+    //This query function will provide a table of the products for the user
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
         console.table(results);
         inquirer
             .prompt([
                 {
+                    //The first inquire question asking for the ID of the product the user wants to choose
                     name: "selection",
                     type: "input",
                     message: "Enter the ID for the product you would like to buy: ",
@@ -37,6 +42,7 @@ function start() {
                     }
                 },
                 {
+                    //The second inquire question asking for the quantity the user would like to choose based on the ID they selected during the previous question
                     name: "quantity",
                     type: "input",
                     message: "How many would you like to buy?",
@@ -49,11 +55,17 @@ function start() {
                 }
             ])
             .then(function(answer) {
+                //This will compare the quantity for the ID selected by the user with the quantity in the products table of the database
                 if (results[answer.selection - 1].stock_quantity > parseInt(answer.quantity)) {
                     var query = "UPDATE products SET stock_quantity = stock_quantity - (?) WHERE ID = ?;"
                     connection.query(query,[answer.quantity, answer.selection], function(err) {
+                        if (err) throw err;
                         console.log("Thank you! Your order has been placed!\n" +
-                        "Your total price is: $" + answer.quantity * results[answer.selection - 1].price);
+                        "Your total price is: $" + answer.quantity * results[answer.selection - 1].price + "\nHere is the updated inventory.");
+                        connection.query("SELECT * FROM products", function(err, results) {
+                            if (err) throw err;
+                            console.table(results);
+                        });
                         connection.end();
                     }
                     )
